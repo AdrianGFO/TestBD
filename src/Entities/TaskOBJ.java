faeb171218fd7df517608cd10ad453353f50f5e3
AdrianGFO <adriangabriel.fo1@gmail.com>
2024-10-07 13:22:19 -0300

    Second Commit

--- src/Entities/TaskOBJ.java
+++ src/Entities/TaskOBJ.java
@@ -1,9 +1,57 @@
 package Entities;
 
-import java.time.*;
+import java.time.LocalDate;
 
 public class TaskOBJ {
 
     private String task;
-    Private LocalDateTime
+    private LocalDate tempo;
+    private ENUM priority;
+
+    
+    public TaskOBJ(String task, LocalDate tempo, ENUM priority) {
+        this.task = task;
+        this.tempo = tempo;
+        this.priority = priority;
+    }
+
+
+    public String getTask() {
+        return task;
+    }
+
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+
+    public LocalDate getTempo() {
+        return tempo;
+    }
+
+
+    public void setTempo(LocalDate tempo) {
+        this.tempo = tempo;
+    }
+
+
+    public ENUM getPriority() {
+        return priority;
+    }
+
+
+    public void setPriority(ENUM priority) {
+        this.priority = priority;
+    }
+
+
+    @Override
+    public String toString() {
+        return "TaskOBJ [Tarefa" + getTask() + "\nData: " + getTempo() + "\nPrioridade: " + getPriority()
+                + "]";
+    }
+
+    
+    
 }
--- src/Program/App.java
+++ src/Program/App.java
@@ -3,24 +3,63 @@ package Program;
 import java.util.Scanner;
 import Entities.ENUM;
 import java.util.ArrayList;
+import Entities.TaskOBJ;
+import java.time.LocalDate;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        ArrayList<Object> TaskList = new ArrayList<Object>;
 
-        Scanner rogerio = new Scanner(System.in);
+        ArrayList <TaskOBJ> TaskList = new ArrayList <TaskOBJ>();
         
+        Scanner rogerio = new Scanner(System.in);
+        int continueAdd;
+       
+        do{    
         System.out.print("Digite sua tarefa: ");
 
         String tasks = rogerio.nextLine();
 
-        System.out.println("Níveis de Prioridade: (Baixa, Média, Alta)");
+        System.out.println("Níveis de Prioridade: \n1 - Alta \n2 - Média \n3 - Baixa)");
         System.out.println("Prioridade da Tarefa: ");
 
-        String priority = rogerio.next();
+        int switchCaseNumber = rogerio.nextInt();
+        ENUM priority = ENUM.Alta; 
+
+        switch(switchCaseNumber){
+            case 1: 
+            priority = ENUM.Alta;
+            break;
+            case 2: 
+            priority = ENUM.Média;
+            break;
+            case 3:
+            priority = ENUM.Baixa;
+            break;
+        }
+
+        LocalDate data = LocalDate.now();
+
+        TaskOBJ myTask = new TaskOBJ(tasks, data, priority);
+        
+        TaskList.add(myTask);
 
 
+        System.out.println("Continuar adicionando Tarefas? (1 - SIM   2 - NÃO)");
+        continueAdd = rogerio.nextInt();
+        rogerio.nextLine();
 
+        }while(continueAdd == 1);
+
+       /* for(TaskOBJ n : TaskList){
+           System.out.println(TaskList.get(n));
+        }*/ 
+        
+        for(int i = 0; i < TaskList.size(); i++){
+            System.out.println(TaskList.get(i) + "\n_________________________________________________________");
+        }
+        
+
+
+        rogerio.close();
     }
 }
